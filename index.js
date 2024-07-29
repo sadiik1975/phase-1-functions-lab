625dd759b884115f448a626dab49ba64d21278df
Sadeek Been <sadiikbeen@gmail.com>
2024-07-29 14:32:55 -0400

    adding update

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+// index.js
+
+function distanceFromHqInBlocks(someValue) {
+    const hq = 42; // Assume headquarters is at 42nd street
+    return Math.abs(someValue - hq);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264; // Each block is 264 feet
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance <= 400) {
+      return 0; // Free sample
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; // Flat rate for distances over 2000 feet
+    } else {
+      return 'cannot travel that far'; // No rides over 2500 feet
+    }
+  }
+  
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+  };
+  
